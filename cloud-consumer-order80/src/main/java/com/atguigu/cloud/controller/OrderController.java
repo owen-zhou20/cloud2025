b918ed31b23390ab485d19e44f953935ed564333
owen-zhou20 <78416546+owen-zhou20@users.noreply.github.com>
2024-04-20 16:17:19 +1000

    feat: add LoadBalancer

--- cloud-consumer-order80/src/main/java/com/atguigu/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/controller/OrderController.java
@@ -3,10 +3,14 @@ package com.atguigu.cloud.controller;
 import com.atguigu.cloud.entities.PayDTO;
 import com.atguigu.cloud.resp.ResultData;
 import jakarta.annotation.Resource;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -25,5 +29,31 @@ public class OrderController {
     @GetMapping("/consumer/pay/get/{id}")
     public ResultData getPayInfo(@PathVariable("id") Integer id){
         return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/"+id, ResultData.class, id);
+    }
+
+    @GetMapping(value = "/consumer/pay/get/info")
+    private String getInfoByConsul()
+    {
+        return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/info", String.class);
+    }
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+    @GetMapping("/consumer/discovery")
+    public String discovery()
+    {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            System.out.println(element);
+        }
+
+        System.out.println("===================================");
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        for (ServiceInstance element : instances) {
+            System.out.println(element.getServiceId()+"\t"+element.getHost()+"\t"+element.getPort()+"\t"+element.getUri());
+        }
+
+        return instances.get(0).getServiceId()+":"+instances.get(0).getPort();
     }
 }
