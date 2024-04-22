8ecb55064d2d178adc9b49073f09bf86a59dce30
owen-zhou20 <78416546+owen-zhou20@users.noreply.github.com>
2024-04-22 16:50:45 +1000

    feat: add BulkHead in Resilience4j CircuitBreaker

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayCircuitController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayCircuitController.java
@@ -10,13 +10,35 @@ import java.util.concurrent.TimeUnit;
 public class PayCircuitController
 {
     //=========Resilience4j CircuitBreaker 的例子
-    @GetMapping(value = "/pay/circuit/{id}")
-    public String myCircuit(@PathVariable("id") Integer id)
+//    @GetMapping(value = "/pay/circuit/{id}")
+//    public String myCircuit(@PathVariable("id") Integer id)
+//    {
+//        if(id == -4) throw new RuntimeException("----circuit id 不能负数");
+//        if(id == 9999){
+//            try { TimeUnit.SECONDS.sleep(5); } catch (InterruptedException e) { e.printStackTrace(); }
+//        }
+//        return "Hello, circuit! inputId:  "+id+" \t " + IdUtil.simpleUUID();
+//    }
+
+    //=========Resilience4j bulkhead 的例子
+    @GetMapping(value = "/pay/bulkhead/{id}")
+    public String myBulkhead(@PathVariable("id") Integer id)
     {
-        if(id == -4) throw new RuntimeException("----circuit id 不能负数");
-        if(id == 9999){
+        if(id == -4) throw new RuntimeException("----bulkhead id 不能-4");
+
+        if(id == 9999)
+        {
             try { TimeUnit.SECONDS.sleep(5); } catch (InterruptedException e) { e.printStackTrace(); }
         }
-        return "Hello, circuit! inputId:  "+id+" \t " + IdUtil.simpleUUID();
+
+        return "Hello, bulkhead! inputId:  "+id+" \t " + IdUtil.simpleUUID();
+    }
+
+
+    //=========Resilience4j ratelimit 的例子
+    @GetMapping(value = "/pay/ratelimit/{id}")
+    public String myRatelimit(@PathVariable("id") Integer id)
+    {
+        return "Hello, myRatelimit欢迎到来 inputId:  "+id+" \t " + IdUtil.simpleUUID();
     }
 }
